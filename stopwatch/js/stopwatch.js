9383c61bc5ec23bb81cc9a6b748b209d85d98fd2
kodama wataru <challenge.spirit.2120@gmail.com>
2019-11-22 15:06:59 +0900

    JSでストップウォッチのスタート、ストップ、リセット機能を追加

--- stopwatch/js/stopwatch.js
+++ stopwatch/js/stopwatch.js
@@ -1,13 +1,40 @@
 $(function() {
   'use strict';
 
+  var
+  minute,
+  second,
+  millisecond,
+  defaultMillitime = 100,
+  defaultTime = 0 * defaultMillitime,
+  time = defaultTime,
+  checkStopwatch = false,
+  $stopwatch = $('#stopwatch');
+
+  setTime();
+
   $('#start').on('click', function() {
-    console.log('start');
+    if(checkStopwatch === false) {
+      checkStopwatch = setInterval(function() {
+        time++;
+        setTime();
+      }, 10);
+    }
   }); 
   $('#stop').on('click', function() {
-    console.log('stop');
+    clearInterval(checkStopwatch);
+    checkStopwatch = false;
   }); 
   $('#reset').on('click', function() {
-    console.log('reset');
+    clearInterval(checkStopwatch);
+    time = defaultTime;
+    setTime();
+    checkStopwatch = false;
   }); 
-})
+  function setTime() {
+    minute = ("0" + Math.floor((time / defaultMillitime) / 60)).slice(-2);
+    second = ("0" + Math.floor(time / defaultMillitime) % 60).slice(-2);
+    millisecond = ("0" + time % defaultMillitime).slice(-2);
+    $stopwatch.text(minute + ":" + second + "." + millisecond);
+  };
+});
