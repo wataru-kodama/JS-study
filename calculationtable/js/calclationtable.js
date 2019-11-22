ebb00564a8e807b4772c903bc7989b2d0918f3c9
kodama wataru <challenge.spirit.2120@gmail.com>
2019-11-22 18:05:58 +0900

    jsで計算表を追加

---
+++ calculationtable/js/calclationtable.js
@@ -0,0 +1,27 @@
+$(function() {
+  'use strict';
+  var
+  $calculation = $('#calculation');
+
+  // for(var j = 0; j < 10; j++) {
+  //   if(j === 0) {
+  //     $('#line1').append($('<th>'));
+  //   }else{
+  //     $('#line1').append($('<th>').text(i));
+  //   };
+  // };
+  for(var i = 0; i < 10; i++) {
+    $calculation.append($('<tr>').attr('id','line' + i));
+    for(var j = 0; j < 10; j++) {
+      if(i === 0 && j ===0) {
+        $('#line' + i).append($('<th>'));
+      }else if(i === 0 && j !== 0) {
+        $('#line' + i).append($('<th>').text(j));
+      }else if(i !== 0 && j === 0) {
+        $('#line' + i).append($('<th>').text(i));
+      }else {
+        $('#line' + i).append($('<td>'));
+      };
+    };
+  }
+});
