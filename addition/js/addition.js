cc8c71048421731141287b32c6f8f906988dca5e
kodama wataru <challenge.spirit.2120@gmail.com>
2019-11-22 23:30:12 +0900

    for文を使って計算式を記述

--- addition/js/addition.js
+++ addition/js/addition.js
@@ -3,7 +3,10 @@ $(function() {
   $addition = $('#addition');
 
   $addition.append($('<tr>').attr('id','line1'));
-  $('#line1').append($('<td>').text('1+1=2'));
-  $('#line1').append($('<td>').text('1+2=3'));
-  $('#line1').append($('<td>').text('1+3=4'));
+  // $('#line1').append($('<td>').text('1+1=2'));
+  // $('#line1').append($('<td>').text('1+2=3'));
+  // $('#line1').append($('<td>').text('1+3=4'));
+  for(var i = 1; i < 10; i++) {
+    $('#line1').append($('<td>').text('1+' + i))
+  }
 });
