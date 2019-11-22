e2fbb6279544c3e3b21e0bd7fa4b799aacea3fda
kodama wataru <challenge.spirit.2120@gmail.com>
2019-11-22 23:25:10 +0900

    JSでtrタグの追加,足し算を記述

--- addition/js/addition.js
+++ addition/js/addition.js
@@ -3,4 +3,7 @@ $(function() {
   $addition = $('#addition');
 
   $addition.append($('<tr>').attr('id','line1'));
+  $('#line1').append($('<td>').text('1+1=2'));
+  $('#line1').append($('<td>').text('1+2=3'));
+  $('#line1').append($('<td>').text('1+3=4'));
 });
