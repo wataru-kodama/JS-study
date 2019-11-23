4b87b2a20f530c0f44e535567ef67f399ec8acc1
kodama wataru <challenge.spirit.2120@gmail.com>
2019-11-23 09:47:20 +0900

    JSで計算式を3行追加

--- multiplication_practice/js/multiplication_practice.js
+++ multiplication_practice/js/multiplication_practice.js
@@ -9,4 +9,14 @@ $(function() {
     result = 1 * i;
     $('#line1').append($('<td>').text('1*' + i + '=' + result));
   };
+  $multiplication.append($('<tr>').attr('id','line2'));
+  for(var i = 1; i < 10; i++) {
+    result = 2 * i;
+    $('#line2').append($('<td>').text('2*' + i + '=' + result));
+  };
+  $multiplication.append($('<tr>').attr('id','line3'));
+  for(var i = 1; i < 10; i++) {
+    result = 3 * i;
+    $('#line3').append($('<td>').text('3*' + i + '=' + result));
+  };
 });
