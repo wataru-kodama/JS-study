9ad31b262e00e62714eacb179c0191f887f9ea39
kodama wataru <challenge.spirit.2120@gmail.com>
2019-11-23 09:34:02 +0900

    JSでtdタグを追加,計算式を記述

--- multiplication_practice/js/multiplication_practice.js
+++ multiplication_practice/js/multiplication_practice.js
@@ -4,4 +4,8 @@ $(function() {
   $multiplication = $('#multiplication');
 
   $multiplication.append($('<tr>').attr('id','line1'));
+  $('#line1').append($('<td>').text('1*1'));
+  $('#line1').append($('<td>').text('1*2'));
+  $('#line1').append($('<td>').text('1*3'));
+  $('#line1').append($('<td>').text('1*4'));
 });
