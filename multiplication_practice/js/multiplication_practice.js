72fff2503787ad4e8b6907e70f41cf9bc579dd35
kodama wataru <challenge.spirit.2120@gmail.com>
2019-11-23 09:38:41 +0900

    for文で計算式を1行追加

--- multiplication_practice/js/multiplication_practice.js
+++ multiplication_practice/js/multiplication_practice.js
@@ -4,8 +4,11 @@ $(function() {
   $multiplication = $('#multiplication');
 
   $multiplication.append($('<tr>').attr('id','line1'));
-  $('#line1').append($('<td>').text('1*1'));
-  $('#line1').append($('<td>').text('1*2'));
-  $('#line1').append($('<td>').text('1*3'));
-  $('#line1').append($('<td>').text('1*4'));
+  // $('#line1').append($('<td>').text('1*1'));
+  // $('#line1').append($('<td>').text('1*2'));
+  // $('#line1').append($('<td>').text('1*3'));
+  // $('#line1').append($('<td>').text('1*4'));
+  for(var i = 1; i < 10; i++) {
+    $('#line1').append($('<td>').text('1*' + i + '='));
+  };
 });
