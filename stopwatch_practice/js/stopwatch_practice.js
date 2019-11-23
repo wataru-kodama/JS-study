5ef71aa0ce6240f940cb75030c82374110fc33e3
kodama wataru <challenge.spirit.2120@gmail.com>
2019-11-23 19:04:05 +0900

    startボタンを押した時の機能を追加

--- stopwatch_practice/js/stopwatch_practice.js
+++ stopwatch_practice/js/stopwatch_practice.js
@@ -9,13 +9,13 @@ $(function() {
   time = defaultTime,
   $stopwatch = $('#stopwatch');
 
-  minute = ("0" + Math.floor(time / 60000)).slice(-2);
-  second = ("0" + Math.floor((time / 1000)) % 60).slice(-2);
-  millisecond = ("0" + time % 10000);
-  $stopwatch.text(minute + ":" + second + "." + millisecond);
+  setTime();
 
   $('#start').on('click', function() {
-    console.log(1);
+    setInterval(function() {
+      time++;
+      setTime();
+    },10);
   });
   $('#stop').on('click', function() {
     console.log(2);
@@ -23,4 +23,10 @@ $(function() {
   $('#reset').on('click', function() {
     console.log(3);
   });
+  function setTime() {
+    minute = ("0" + Math.floor(time / 6000)).slice(-2);
+    second = ("0" + Math.floor((time / 100)) % 60).slice(-2);
+    millisecond = ("0" + time % 1000).slice(-2);
+    $stopwatch.text(minute + ":" + second + "." + millisecond);
+  };
 });
