dd18bb1f6069e00e95c1c37fa880aab0e1ef121b
kodama wataru <challenge.spirit.2120@gmail.com>
2019-11-25 13:18:29 +0900

    リセットの機能の修正

--- calculationtable/js/calclationtable.js
+++ calculationtable/js/calclationtable.js
@@ -40,8 +40,8 @@ $(function() {
   });
   $('#reset').on('click', function() {
     for(i = 1; i < 10; i++) {
-      for(j = 1; j < 10; j++) {
-        $('#line' + i).find('td').eq(j - 1).text("");
+      for(j = 0; j < 9; j++) {
+        $('#line' + i).find('td').eq(j).text("");
       };
     };
   });
