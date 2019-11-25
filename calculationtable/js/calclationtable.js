5b230cef1e99c6d85486442a4c193b5e6730aca1
kodama wataru <challenge.spirit.2120@gmail.com>
2019-11-25 14:39:07 +0900

    共通処理の関数化を追加

--- calculationtable/js/calclationtable.js
+++ calculationtable/js/calclationtable.js
@@ -1,48 +1,58 @@
 $(function() {
   'use strict';
   var
-  $calculation = $('#calculation');
+  $calculation = $('#calculation'),
+  i,
+  j,
+  calc,
+  Formula;
 
-  // for(var j = 0; j < 10; j++) {
-  //   if(j === 0) {
-  //     $('#line1').append($('<th>'));
-  //   }else{
-  //     $('#line1').append($('<th>').text(i));
-  //   };
-  // };
-  for(var i = 0; i < 10; i++) {
+  for(i = 0; i < 10; i++) {
     $calculation.append($('<tr>').attr('id','line' + i));
-    for(var j = 0; j < 10; j++) {
+    for(j = 0; j < 10; j++) {
       if(i === 0 && j ===0) {
-        $('#line' + i).append($('<th>'));
+        $('#line' + i).append($('<td>'));
       }else if(i === 0 && j !== 0) {
-        $('#line' + i).append($('<th>').text(j));
+        $('#line' + i).append($('<td>').text(j));
       }else if(i !== 0 && j === 0) {
-        $('#line' + i).append($('<th>').text(i));
+        $('#line' + i).append($('<td>').text(i));
       }else {
         $('#line' + i).append($('<td>'));
       };
     };
   };
   $('#addition').on('click', function() {
-    for(i = 1; i < 10; i++) {
-      for(j = 1; j < 10; j++) {
-        $('#line' + i).find('td').eq(j - 1).text(i+j);
-      };
-    };
+    setCalclation('addition')
   });
   $('#multiplication').on('click', function() {
-    for(i = 0; i < 10; i++) {
-      for(j = 1; j < 10; j++) {
-        $('#line' + i).find('td').eq(j - 1).text(i*j);
-      };
-    };
+    setCalclation('multiplication')
   });
   $('#reset').on('click', function() {
+    setCalclation('reset');
+  });
+
+  function setCalclation(kind) {
     for(i = 1; i < 10; i++) {
-      for(j = 0; j < 9; j++) {
-        $('#line' + i).find('td').eq(j).text("");
+      for(j = 1; j < 10; j++) {
+        switch(kind) {
+          case 'addition':
+            calc = i + j;
+            Formula = '+';
+            break;
+          case 'multiplication':
+            calc = i * j;
+            Formula = '×';
+            break;
+          case 'reset':
+           calc = '';
+           Formula = '';
+           break;
+          default:
+            break;
+        };
+        $('#line' + i).find('td').eq(j).text(calc);
       };
     };
-  });
+    $('#line0').find('td').eq(0).text(Formula);
+  };
 });
