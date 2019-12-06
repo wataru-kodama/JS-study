3c30b45613d2cee90d892420c1b192890783143d
wataru kodama <challenge.spirit.2120@gmail.com>
2019-12-06 15:54:28 +0900

    STARTボタンを押した時の挙動を追加

--- bingo_game/js/bingo.js
+++ bingo_game/js/bingo.js
@@ -1,13 +1,27 @@
 $(function() {
   'use strict';
-var
-  i,
-  numMax = 50,
-  numList = [];
-for(var i = 1; i <= numMax; i++) {
-  numList.push(i);
-  $('#numberarea').append($('<li>').attr('id', 'num' + i).text(i));
-};
-console.log(numList);
-$('#result').text(1);
+  var
+    i,
+    numMax = 50,
+    numList = [],
+    roulette,
+    number;
+  for(var i = 1; i <= numMax; i++) {
+    numList.push(i);
+    $('#numberarea').append($('<li>').attr('id', 'num' + i).text(i));
+  };
+
+  $('#result').text(1);
+  $('#startbtn').on('click', function() {
+    roulette = setInterval(function() {
+      number = Math.floor(Math.random() * numList.length);
+      $('#result').text(numList[number]);
+    }, 10);
+    $('#startbtn').hide();
+    $('#stopbtn').show();
+  });
+  $('#stopbtn').on('click', function() {
+    $('#startbtn').show();
+    $('#stopbtn').hide();
+  });
 });
