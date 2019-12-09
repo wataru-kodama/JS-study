10372f8e5bdaf25a042b87361e12330ef7a49d06
wataru kodama <challenge.spirit.2120@gmail.com>
2019-12-09 15:23:22 +0900

    ボタン押した時のサウンド追加

--- bingo_game/js/bingo.js
+++ bingo_game/js/bingo.js
@@ -6,28 +6,49 @@ $(function() {
     numList = [],
     roulette,
     number,
-    result = 0;
-  for(var i = 1; i <= numMax; i++) {
+    result = 0,
+    $start = $('#startsound'),
+    $stop = $('#stopsound'),
+    start_Time = 7 * 1000,
+    roulette_Count = 0,
+    roulette_Time = 0;
+    
+  for(i = 1; i <= numMax; i++) {
     numList.push(i);
     $('#numberarea').append($('<li>').attr('id', 'num' + i).text(i));
   };
 
-  $('#result').text(1);
+  $('#result').text(0);
   $('#startbtn').on('click', function() {
     roulette = setInterval(function() {
       number = Math.floor(Math.random() * numList.length);
       $('#result').text(numList[number]);
+      roulette_Count++;
+      roulette_Time = roulette_Count * 10;
+      if(roulette_Time === start_Time) {
+        stopRoulette();
+      }
     }, 10);
     $('#startbtn').hide();
     $('#stopbtn').show();
+    $start.trigger('play');
+    $stop.trigger('pause');
+    $stop[0].currentTime = 0;
   });
   
   $('#stopbtn').on('click', function() {
+    stopRoulette();
+  });
+  function stopRoulette() {
     clearInterval(roulette);
+    roulette_Count = 0;
     result = numList[number];
     numList.splice(number, 1);
     $('#numberarea').find('li').eq(parseInt(result, 10) - 1).addClass('hitnum');
     $('#startbtn').show();
     $('#stopbtn').hide();
-  });
+    $start.trigger('pause');
+    $start[0].currentTime = 0;
+    $stop.trigger('play');
+  };
 });
