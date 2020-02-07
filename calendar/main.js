78224b5ed4f3d440b290403f3451606a9d6ce73d
wataru kodama <challenge.spirit.2120@gmail.com>
2020-02-07 17:52:43 +0900

    todayクラスをつける値を修正

--- calendar/main.js
+++ calendar/main.js
@@ -11,33 +11,33 @@ $(function() {
     }
 
   var
-    setDay = new Date(),
-    today = setDay.getDate(),
-    setYear = setDay.getFullYear(),
-    setMonth = setDay.getMonth(),
-    startDay = new Date(setYear, setMonth, 1),
-    lastDay = new Date(setYear, setMonth + 1, 0),
+    set_Day = new Date(),
+    today = set_Day.getDate(),
+    set_Year = set_Day.getFullYear(),
+    set_Month = set_Day.getMonth(),
+    startDay = new Date(set_Year, set_Month, 1),
+    lastDay = new Date(set_Year, set_Month + 1, 0),
     startDay_Week = startDay.getDay(),
     month_lastDay = lastDay.getDate(),
     textSkip = true,
     textDate = 1,
     tableBody ='',
-    i,
-    j;
-
-  $('.title').append($('<h1>').text(setYear + '年 ' + (setMonth + 1) + '月'));
-  for(var i = 0; i < 5; i++) {
+    col,
+    low;
+    
+  $('.title').append($('<h1>').text(set_Year + '年 ' + (set_Month + 1) + '月'));
+  for(col = 0; col < 5; col++) {
     var $tr = '<tr>';
-    for(var j = 0; j < 7; j++) {
-      if(i === 0 && startDay_Week === j) {
+    for(low = 0; low < 7; low++) {
+      if(col === 0 && startDay_Week === low) {
         textSkip = false;
       }
       if(textDate > month_lastDay) {
         textSkip = true;
       }
       var 
-        textTd = textSkip ? ' ' : textDate++,
         addClass = today === textDate ? 'today': '',
+        textTd = textSkip ? ' ' : textDate++,
         $td = '<td class='+addClass+'>'+textTd+'</td>';
       $tr += $td;
     }
